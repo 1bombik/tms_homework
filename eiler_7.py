a1b3c58e950110354318f85e409fca9f91bf7395
Dmitry <116752796+1bombik@users.noreply.github.com>
2022-10-31 19:40:26 +0300

    Update eiler_7.py

--- eiler_7.py
+++ eiler_7.py
@@ -1,7 +1,7 @@
 number = int(input("Какое по счёту простое число тебе нужно? - "))
 y = []
 x = 2
-for b in range(1,50):
+for b in range(1,100000):
     c = (x**(b-1))-1
     if c % b == 0:
         y.append(b)
