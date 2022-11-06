7d417183419045d791312642e7dfed47ff8aa2d9
Dmitry <116752796+1bombik@users.noreply.github.com>
2022-11-06 21:32:30 +0300

    Add files via upload

--- digits.py
+++ digits.py
@@ -1,18 +1,14 @@
 import random as r
 
-a = int(input("Enter your first digit from 1 to 20 - "))
-b = int(input("Enter your second digit from 1 to 20 - "))
-compare = int(input("How many times to compare? - "))
-c = r.randint(1,20)
-d = r.randint(1,20)
-i = 1
-ab = 0 #if ab is greater than cd
-cd = 0 #if cd is greater than ab
-for i in range(1, compare+1):
-    if a + b > c + d:
-        ab += 1
-        c = r.randint(1,20)
-        d = r.randint(1,20)
-    else:
-        cd += 1
-print(ab, cd)
+amount = int(input("How much numbers? - "))
+digit = str(input("What digit to find? - "))
+l_numbers = [] #list with random numbers
+random_number = ""
+for i in range(1, amount + 1):
+    random_number = str(r.randint(1,100))
+    l_numbers.append(random_number)
+    i += 1
+string = "".join(l_numbers)
+times = string.count(digit)
+print("The random numbers are - ", l_numbers, end="\n" +
+"Your digit was found " + str(times) + " times")
